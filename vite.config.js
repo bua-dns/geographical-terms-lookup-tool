2fa2bc23343a2294973ab08c617fd5ce34a04728
culturetogo <m.mueller@culture-to-go.de>
2025-05-31 18:59:10 +0200

    WIP: preparations for deployment on github pages

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/anatomy-terms-lookup-tool/'
+  base: '/geographical-terms-lookup-tool/'
 })
